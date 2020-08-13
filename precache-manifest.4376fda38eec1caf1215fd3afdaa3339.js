d25f2479e7f499b02d6f5b2bf96f7eab75832fad
mutuajoseph <mutuajoseph17@gmail.com>
2020-08-13 23:33:24 +0300

    Updates

---
+++ precache-manifest.4376fda38eec1caf1215fd3afdaa3339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87125f51491d38700d2d936f6ece3705",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "ac76eaed2279e7ba977d",
+    "url": "/Monster-Rolodex/static/css/main.7106a65a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac76eaed2279e7ba977d",
+    "url": "/Monster-Rolodex/static/js/main.b9a1e753.chunk.js"
+  },
+  {
+    "revision": "2b55c592b5ec82aad6ec",
+    "url": "/Monster-Rolodex/static/js/runtime-main.28f6930b.js"
+  }
+]);
